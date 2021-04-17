098ad2fcdf143d2412cdd5e89efa2e80efaf70c2
Yuqing_Chen <chenyuqing0429@foxmail.com>
2021-04-18 01:57:22 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Open Hack Art</h1>
+  <div>@scyq</div>
+  <a href="https://github.com/scyq">GitHub</a>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
